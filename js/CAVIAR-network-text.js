6d2b5b0338a5227590f441f9179e02fb5060eaaa
M Powers <mp@mpowers.info>
2021-04-25 03:23:18 -0400

    Add explanatory text to phases

---
+++ js/CAVIAR-network-text.js
@@ -0,0 +1,46 @@
+textPhase = [
+    `
+    Marijuana is imported to Canada from Morocco, transiting through Spain.  (Hover over key players for information.  Drag nodes to reposition.  Right-click to mark/unmark nodes to track through phases.)
+    `,
+
+    `
+    Additional wiretaps are authorized, adding nodes to the graph.
+    `,
+
+    `
+    Additional wiretaps are authorized, adding nodes to the graph.
+
+    `,
+
+    `
+    Seizure of 300 kg of marijuana.
+    `,
+
+    `
+    Presumably in response to marijuana seizure in previous phase, traffickers diversify and reorient to import cocaine from Colombia, transiting through the United States.  n12 enters the graph as the primary contact for the new operation.
+    `,
+
+    `
+    n1 begins to delegate contacts to n3.  Three seizures, totaling 30 kg of marijuana and 2 kg of cocaine.
+    `,
+
+    `
+    n12 separates from main network, presumably in response to cocaine seizure in previous phase.  Seizure of 401 kg of marijuana.
+    `,
+
+    `
+    Seizure of 9 kg of cocaine.
+    `,
+
+    `
+    Two seizures: 500 kg of marijuana and 2 kg of cocaine.
+    `,
+
+    `
+    n12 separates from main network, presumably in response to cocaine seizure in previous phase.  Seizure of 2200 kg of marijuana.
+    `,
+
+    `
+    Two seizures, totaling 23 kg of cocaine.  Enterprise is now highly compartmentalized (compare to Phase 1.)  n41 emerges as a key player.
+    `,
+]
